db7739c1f9576977778bb34155d623a6c8cd9269
Matt Marcum <matthew.marcum@activenetwork.com>
2016-03-21 16:22:03 -0500

    updated getLatestPods to be more yieldy

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,15 +2,10 @@ import Ember from 'ember';
 import { task } from 'ember-concurrency';
 
 export default Ember.Controller.extend({
-
   getLatestPods: task(function * (){
-    yield this.store.findAll('podcast')
-      .then(
-        podcasts => Ember.RSVP.all(
-          podcasts.map(
-            podcast => podcast.update()
-          )
-        )
-      );
+    let podcasts = yield this.store.findAll('podcast')
+    let updates = yield Ember.RSVP.all(podcasts.map( podcast => podcast.update() ));
+
+    return updates;
   })
 });
